1f2ddd60ae8368bfba1356d45a0ab99b80a98f78
itspauloroberto <itspauloroberto@gmail.com>
2025-08-06 11:46:37 -0300

    feat: improving build to maximize compatibility

---
+++ tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  target: "es2020",
+  dts: true,
+  minify: true,
+  sourcemap: false,
+  clean: true,
+  outDir: "dist",
+  skipNodeModulesBundle: true,
+  external: ["react", "react-dom"],
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".cjs" : ".mjs"
+    };
+  },
+  loader: {
+    ".css": "copy"
+  }
+});
